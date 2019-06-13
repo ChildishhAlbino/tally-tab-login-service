90ae79edf143a77822c22568a45e4cea9b4a4b7f
Connor <connorwilliams.home@gmail.com>
2019-06-13 17:25:07 +1000

    [Connor] Improve validation of data entering system.

--- api/models/user.js
+++ api/models/user.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
-	email: String,
-	passwordHash: String
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+	},
+	passwordHash: { type: String, required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
--- api/routes/signupRoutes.js
+++ api/routes/signupRoutes.js
@@ -4,34 +4,33 @@ const User = require('../models/user');
 const mongoose = require('mongoose');
 
 router.post('/', (req, res, next) => {
-	User.find({ email: req.body.email })
-		.exec()
-		.then((doc) => {
-			console.log(doc);
-			if (doc.length != 0) {
-				return res.status(500).json({
-					message: 'Email already exits in database.'
-				});
-			} else {
-				const user = new User({
-					_id: new mongoose.Types.ObjectId(),
-					email: req.body.email,
-					passwordHash: req.body.hash
-				});
-				result = user.save();
-				res.status(201).json({
-					message: 'Handling sign-up request',
-					user: user
-				});
-			}
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json({
-				message: 'Error while signing up.',
-				error: err
+	User.find({ email: req.body.email }).exec().then((doc) => {
+		if (doc.length >= 1) {
+			return res.status(409).json({
+				message: 'Email already exits in database.'
 			});
-		});
+		} else {
+			const user = new User({
+				_id: new mongoose.Types.ObjectId(),
+				email: req.body.email,
+				passwordHash: req.body.hash
+			});
+			user
+				.save()
+				.then((result) => {
+					res.status(201).json({
+						message: 'Handling sign-up request',
+						user: user
+					});
+				})
+				.catch((err) => {
+					res.status(500).json({
+						message: 'Error while signing up.',
+						error: err
+					});
+				});
+		}
+	});
 });
 
 module.exports = router;
