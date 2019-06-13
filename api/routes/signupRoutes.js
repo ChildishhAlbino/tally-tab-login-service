a3d9ac8e53e33a782902ed13969141de577b71cf
Connor <connorwilliams.home@gmail.com>
2019-06-13 18:07:26 +1000

    [Connor] Improve returned data on /signup

--- api/routes/signupRoutes.js
+++ api/routes/signupRoutes.js
@@ -4,6 +4,13 @@ const User = require('../models/user');
 const mongoose = require('mongoose');
 
 router.post('/', (req, res, next) => {
+	/* Takes a email and password and hashes it with BCrypt's
+	hash function. Salted 10 Times.
+
+	Returns 201 if everything worked successfully.
+	Returns 409 if the email supplied already exists in the database.
+	Returns 500 if an error occurred during the process.
+	*/
 	User.find({ email: req.body.email }).exec().then((doc) => {
 		if (doc.length >= 1) {
 			return res.status(409).json({
@@ -20,7 +27,13 @@ router.post('/', (req, res, next) => {
 				.then((result) => {
 					res.status(201).json({
 						message: 'Handling sign-up request',
-						user: user
+						createdUser: user._id,
+						requests: [
+							{ type: 'GET', url: `localhost:3000/users/${user._id}`, verificationRequired: false },
+							{ type: 'PATCH', url: `localhost:3000/users/${user._id}`, verificationRequired: true },
+							{ type: 'DELETE', url: `localhost:3000/users/${user._id}`, verificationRequired: true },
+							{ type: 'POST', url: `localhost:3000/login`, verificationRequired: false }
+						]
 					});
 				})
 				.catch((err) => {
