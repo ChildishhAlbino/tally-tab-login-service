5f65b8e37da0f208ea2cc60fbb04a2fcd3fadb3a
Connor <connorwilliams.home@gmail.com>
2019-06-14 15:23:13 +1000

    [Connor] Add password hashing to signup process.

--- api/routes/signupRoutes.js
+++ api/routes/signupRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 router.post('/', (req, res, next) => {
 	/* Takes a email and password and hashes it with BCrypt's
 	hash function. Salted 10 Times.
-
+	--------------------------------------------------------------
 	Returns 201 if everything worked successfully.
 	Returns 409 if the email supplied already exists in the database.
 	Returns 500 if an error occurred during the process.
@@ -17,31 +18,51 @@ router.post('/', (req, res, next) => {
 				message: 'Email already exits in database.'
 			});
 		} else {
-			const user = new User({
-				_id: new mongoose.Types.ObjectId(),
-				email: req.body.email,
-				passwordHash: req.body.hash
+			bcrypt.hash(req.body.password, 10, (err, hash) => {
+				if (err) {
+					return res.status(500).json({
+						err: err
+					});
+				} else {
+					const user = new User({
+						_id: new mongoose.Types.ObjectId(),
+						email: req.body.email,
+						passwordHash: hash
+					});
+					user
+						.save()
+						.then((result) => {
+							res.status(201).json({
+								message: 'Handling sign-up request',
+								createdUser: user._id,
+								requests: [
+									{
+										type: 'GET',
+										url: `localhost:3000/users/${user._id}`,
+										verificationRequired: false
+									},
+									{
+										type: 'PATCH',
+										url: `localhost:3000/users/${user._id}`,
+										verificationRequired: true
+									},
+									{
+										type: 'DELETE',
+										url: `localhost:3000/users/${user._id}`,
+										verificationRequired: true
+									},
+									{ type: 'POST', url: `localhost:3000/login`, verificationRequired: false }
+								]
+							});
+						})
+						.catch((err) => {
+							res.status(500).json({
+								message: 'Error while signing up.',
+								error: err
+							});
+						});
+				}
 			});
-			user
-				.save()
-				.then((result) => {
-					res.status(201).json({
-						message: 'Handling sign-up request',
-						createdUser: user._id,
-						requests: [
-							{ type: 'GET', url: `localhost:3000/users/${user._id}`, verificationRequired: false },
-							{ type: 'PATCH', url: `localhost:3000/users/${user._id}`, verificationRequired: true },
-							{ type: 'DELETE', url: `localhost:3000/users/${user._id}`, verificationRequired: true },
-							{ type: 'POST', url: `localhost:3000/login`, verificationRequired: false }
-						]
-					});
-				})
-				.catch((err) => {
-					res.status(500).json({
-						message: 'Error while signing up.',
-						error: err
-					});
-				});
 		}
 	});
 });
